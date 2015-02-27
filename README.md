1107a3238156cf093df2724e1956605b685e449c
Hugo Lindström <hugolm84@gmail.com>
2015-02-27 12:53:42 +0100

    Add Registration capabilities

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ curl -ikL -X POST -H "username: username" -H "password: pwd" http://localhost:30
 ## connect
 curl -ikLv -X GET -H "authorization: Bearer TOKENZZZZZZZZ"
 
-
+## register
+curl -ikLv -X POST -H "authorization: Bearer TOKENZZZZZZZ" -H "username: username" -H "password: pwd" http://localhost:3000/register
---
+++ routes/register/register.js
@@ -0,0 +1,25 @@
+var passport = require('passport')
+	Account = require('./../../models/account')
+
+module.exports = {
+  '/register': {
+    post: function(req, res, cb) {
+
+    	req.body = {}
+    	req.body.username = req.headers.username;
+    	req.body.password = req.headers.password;
+
+		Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+			if (err) {
+				res.json({code : 400, msg: "Failed to registered new user", account: account});
+				return cb();
+			}
+
+			passport.authenticate('local')(req, res, function () {
+				res.json({code : 200, msg: "Registered new user"});
+				return cb();
+			});
+		});
+	}
+  }
+};
