57fd2edba574d6f3fb2c6755b92b8f632726bff0
Hugo Lindström <hugolm84@gmail.com>
2015-02-27 13:24:57 +0100

    Cleanup

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var uuid = require('node-uuid')
 	, server = require('./modules/server').server() // Initialize restify server
 	, io = require('./modules/socket').io(server.server) // Initialize socket.io
 	, serve_static = require('./modules/serve_static') // Helper to serve static files
-	, jwt_secret = require('./modules/jwt_secret')
-    , jwt = require('restify-jwt')
 
 // Requires header Authentication: Bearer TOKEN
 server.get('/v1/connect',function(req, res, next) { 
