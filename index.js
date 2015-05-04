ffd40339789545be0e7add8489042542d1a331dc
Hugo Lindström <hugolm84@gmail.com>
2015-05-04 13:26:01 +0200

    Add debug() and join()

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var server = require('./modules/server').server() // Initialize restify server
 	, io = require('./modules/socket').io(server.server) // Initialize socket.io
 	, serve_static = require('./modules/serve_static') // Helper to serve static files
+	, debug = require('debug')('presence:app');
 
 // Requires header Authentication: Bearer TOKEN
 server.get('/v1/connect',function(req, res, next) { 
@@ -8,5 +9,5 @@ server.get('/v1/connect',function(req, res, next) {
 })
 
 server.listen(3000, function () {
-    console.log('socket.io server listening at %s', server.url);
+    debug('socket.io server listening at %s', server.url);
 });
--- modules/socket.js
+++ modules/socket.js
@@ -3,25 +3,40 @@ var socketio = require('socket.io')
     , fs = require('fs')
     , socketioJwt = require('socketio-jwt')
     , jwt_secret = require.main.require('./modules/jwt_secret')
+    , debug = require('debug')('presence:server');
+
+var clientPositions = {};
 
 var self = module.exports = {
     io: function(server) {
-        var io = socketio.listen(server);
+        var io = socketio.listen(server); 
         // Requires Header Authorization: Bearer TOKEN
-        /*io.use(socketioJwt.authorize({
+        io.use(socketioJwt.authorize({
             secret: jwt_secret,
             handshake: true
-        }));*/
+        }));
 
         io.on('connection', function(socket){
 
-            var genUUID = uuid.v4();
-            console.log('a user connected', genUUID);
+            debug('a user connected', socket.id);
             
-            io.emit('connected', genUUID);
+            // Notify all that UUID connected
+            io.emit('connected', socket.id);
+
+            // Send all client positions to a newly connected device
+            for(var key in clientPositions) {
+                if(clientPositions.hasOwnProperty(key)) {
+                    debug("Sending", key, clientPositions[key]);
+                    io.emit('position', key, clientPositions[key]);
+                }
+            }
 
             socket.on('disconnect', function(){
-                console.log('user disconnected', genUUID);
+                debug('user disconnected', socket.id);
+                // Make sure that disconnecting user leaves all joined rooms
+                socket.leaveAll();
+                delete clientPositions[socket.id];
+                io.emit('disconnect', socket.id);
             });
 
             socket.on('message', function(msg){
@@ -29,14 +44,41 @@ var self = module.exports = {
                 io.emit('message', msg);
             });
 
-            socket.on('position', function(id, distance, uuid){
-                console.log('enter:', id, uuid);
-                io.emit('position', id, distance, uuid);
+            /**
+             * Devices send their distance (pre calculated in m) to its closest beacon id
+             * then joins the beacon "room".
+             * Devices can only be linked to one specific room at a time
+             */
+            socket.on('position', function(id, distance){
+                var prevRoom = socket.room;
+                
+                if(prevRoom && prevRoom != id) {
+                    socket.leave(prevRoom, function(err) {
+                        if(err) {
+                            debug("Leaving previous room resulted in error:", err);
+                            return;
+                        } 
+                        debug("Socket ID", socket.id, "left room", socket.room);
+                    });
+                }
+
+                socket.join(id, function(err) {
+                    if(err) {
+                        debug("Joining room", id, "resulted in error:", err);
+                        return;
+                    } 
+
+                    debug("Room", id, "now have",  Object.keys(io.sockets.adapter.rooms[id]).length, "connected devices")
+                    
+                    // Set the new id as current room, emit to listeners that UUID's position changed to beacon ID
+                    // Emitting is mostly for graphical demonstration purposes in this usecase, 
+                    // but could be used to display information to all connected devices
+                    socket.room = id;
+                    clientPositions[socket.id] = {beacon: id, distance: distance};
+                    io.emit('position', socket.id, clientPositions[socket.id]);
+                });
             });
-
         });
-
-
         return io;
     }
 };
