c9138cd1ecac4b676193f25794ee93fe1f23b2d1
Hugo Lindström <hugolm84@gmail.com>
2015-05-06 13:22:00 +0200

    Add GPIO support

--- modules/gpio.js
+++ modules/gpio.js
@@ -1,13 +1,39 @@
 var Gpio = require('onoff').Gpio, 
 	debug = require('debug')('GPIO'),
-  	outs = [new Gpio(67, 'out'), new Gpio(68, 'out'), new Gpio(69, 'out')];
+	// 67, 68, 69
+  	gpios = {}
 
+function writeGPIO(gpio_id, onOff) {
+	if(!gpios.hasOwnProperty(gpio_id)) {
+		gpios[gpio_id] = new Gpio(gpio_id, 'out');
+	}
+	gpios[gpio_id].write(onOff, function(err) {
+		if(err) {
+			debug("Writing GPIO_ID", err);
+		}
+		else {
+			gpios[gpio_id].read(err, value) {
+				if(err) debug("Reading GPIO_ID", err)
+				else debug("GPIO_ID", gpio_id, "is now", (onOff === 0 ? "negative" : "positive"));
+			}
+		}
+	});
+}
 module.exports = {
-	setOnOrOff: function(index) {
-		var gpio = outs[index];
-		var before = gpio.readSync();
-		gpio.writeSync(before^1);
-		var now = gpio.readSync();
-		debug("Gpio was", before, "now it is", now);
+	freeAll: function(cb) {
+		var count = 0;
+		for(var key in gpios) {
+			gpios[key].writeSync(0);
+			gpios[key].unexport();
+			delete gpios[key];
+			count++;
+		}
+		cb(count);
+	},
+	positive: function(gpio_id) {
+		writeGPIO(gpio_id, 1);
+	},
+	negative: function(index) {
+		writeGPIO(gpio_id, 0);
 	}
 }
