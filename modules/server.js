d3b6bdc68c275c34ba294308f7b5d505882635f1
Hugo Lindström <hugolm84@gmail.com>
2015-06-08 10:14:23 +0000

    Clear states on start

--- modules/server.js
+++ modules/server.js
@@ -3,9 +3,17 @@ var restify = require('restify')
     , jwt = require('restify-jwt')
     , jwt_secret = require.main.require('./modules/jwt_secret')
     , passport = require('passport')
+    , debug = require('debug')('presence:server')
+    , Beacon = require.main.require('./models/beacon')
 
 module.exports = {
     server: function() {
+	Beacon.update({}, { state: "negative", num_attached: 0 }, { multi: true }, function (err, raw) {
+        	if (err) debug(err);
+        	else debug("Cleared all beacons", raw);
+        	debug("Ok");
+    	});
+
         var server = restify.createServer();
         // ALL routes requires BEARER Token, except /token and /register
         server.use(jwt({ secret: jwt_secret}).unless({path: ['/v1/token', '/v1/register'] }));
